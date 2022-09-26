79c6b05acf3cee264290e96e782d194dd80a0282
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2022-09-26 18:56:32 -0300

    primer commit

---
+++ 6JavaScript/scripts.js
@@ -0,0 +1,20 @@
+//alert("Hola mundo");
+console.log("Hola Universo!");
+//document.write("Hola Universo!!");
+
+var nombrePersona;
+let edad;
+
+nombrePersona = "Norberto"; //String
+edad = 25; //Number
+mayorDeEdad = false; //Boolean lógicos o booleanos true o false
+
+document.writeln("Hola " + nombrePersona + " tenés " + edad + " años");
+
+document.writeln(edad + nombrePersona); 
+
+console.log(edad * nombrePersona);//NaN Not a Number
+
+console.log(mayorDeEdad);
+
+nombre = prompt("Cuál es tu nombre?");
