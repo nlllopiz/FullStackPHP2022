4cb2b2702e09516de7f5f055be58224ede371507
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2022-09-28 17:34:38 -0300

    clase 2 JS

--- 6JavaScript/clase1/scripts.js
+++ 6JavaScript/clase1/scripts.js
@@ -1,20 +1,24 @@
-//alert("Hola mundo");
+
+alert("Hola mundo");
 console.log("Hola Universo!");
 //document.write("Hola Universo!!");
-
+ 
 var nombrePersona;
-let edad;
+let edad, diaCumple;
+const pi = 3.14159;
 
 nombrePersona = "Norberto"; //String
 edad = 25; //Number
+diaCumple = 18;
+
 mayorDeEdad = false; //Boolean lógicos o booleanos true o false
 
 document.writeln("Hola " + nombrePersona + " tenés " + edad + " años");
 
-document.writeln(edad + nombrePersona); 
+document.writeln(edad + diaCumple); 
 
 console.log(edad * nombrePersona);//NaN Not a Number
 
 console.log(mayorDeEdad);
 
-nombre = prompt("Cuál es tu nombre?");
+prompt("Cuál es tu nombre?");
---
+++ 6JavaScript/clase2/scripts.js
@@ -0,0 +1,21 @@
+console.log("OK");
+
+
+function saludar() {
+
+    let nombreGente;
+
+    nombreGente = document.getElementById("nombreGente").value;
+
+    document.getElementById("saludo").innerHTML = "Hola " + nombreGente;
+
+    document.getElementById("botonSaludar").style.display = "none";
+
+
+}
+
+
+
+
+
+
