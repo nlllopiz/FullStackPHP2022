92716f530e3a56768c96638ed05f6f40a37e8afd
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2022-10-12 18:07:22 -0300

    clase5

--- 6JavaScript/clase4/piedraPapelTijera/scripts.js
+++ 6JavaScript/clase4/piedraPapelTijera/scripts.js
@@ -4,31 +4,68 @@
 3-Tijera
 */
 
-let opcionUsuario;
+let opcionUsuario = prompt("1-Piedra 2-Papel 3-Tijera");
+
+
+//let opcionComputadora = 1;
 
 let opcionComputadora = Math.floor(Math.random() * 3 + 1);
 
-console.log(opcionComputadora);
-/*
+
+let ganoUsuario = 0 , ganoComputadors = 0;
+
+//console.log(opcionComputadora);
+
 if (opcionUsuario == opcionComputadora){
-    empate
+    document.write("<br>Empate");
 } else {
     switch(opcionUsuario) {
         case "1":
             if(opcionComputadora == 2) {
-                Gano Computadora
+                document.write("<br>Tu opción: " + opcionUsuario + "- Piedra");
+                document.write("<br>Computadora: " + opcionComputadora + "- Papel");
+                document.write("<br>Gana Computadora!");
+                ganoComputadora++;
+            } else {  //opcionComputadora == 3
+                document.write("<br>Tu opción: " + opcionUsuario + "- Piedra");
+                document.write("<br>Computadora: " + opcionComputadora + "- Tijera");
+                document.write("<br>Ganaste!!!");
             }
-            else {
-                Ganaste!!
-            }
+            break;
+
         case "2":
             if(opcionComputadora == 1) {
-                Ganaste!!
+                document.write("<br>Tu opción: " + opcionUsuario + "- Papel");
+                document.write("<br>Computadora: " + opcionComputadora + "- Piedra");
+                document.write("<br>Ganaste!!");
+            } else {    //opcionComputadora == 3
+                document.write("<br>Tu opción: " + opcionUsuario + "- Papel");
+                document.write("<br>Computadora: " + opcionComputadora + "- Tijera");
+
+                document.write("<br>Ganó Computadora");
             }
-            else {
-                Ganó Computadora
+            break;
+        case "3":
+            if (opcionComputadora == 1) {
+                document.write("<br>Tu opción: " + opcionUsuario + "- Tijera");
+                document.write("<br>Computadora: " + opcionComputadora + "- Piedra");
+                document.write("<br>Gana Computadora");
+            } else {  //opcion Computadora == 2
+                document.write("<br>Tu opción: " + opcionUsuario + "- Tijera");
+                document.write("<br>Computadora: " + opcionComputadora + "- Papel");
+                document.write("<br>Ganaste!!!");
             }
+            break;
+
+        default:
+            document.write("<br> Opción Incorrecta");
     }
+/*
+    desea salir? (s/n)
+    salir = 
+
+while(salir = "n")
+*/
+
 
 }
-*/
