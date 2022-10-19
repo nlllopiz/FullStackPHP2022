3fcffd6737d5778b80b4269371cf4ee0d414833b
Norberto Llopiz <norberto.llopiz@bue.edu.ar>
2022-10-19 14:44:22 -0300

    clase5

---
+++ 6JavaScript/clase5/scripts.js
@@ -0,0 +1,53 @@
+console.log("clase5jsOK")
+
+let dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+console.log(dias);
+
+console.log(dias[0]);
+
+let ppt = ["Piedra", "Papel", "Tijera"];
+
+console.log(ppt);
+
+let opcionUsuario = 1;
+
+console.log(ppt[opcionUsuario - 1]);
+
+ppt[0] = "Roca";
+
+console.log(ppt);
+
+dias[4] = "";
+
+console.log(dias);
+
+dias[9] = "Osvaldo";
+
+console.log(dias);
+
+console.log(dias[7]);
+
+dias[7] = "Laura";
+console.log(dias);
+
+//
+
+let numeros = [];
+
+for(i=0; i<10; i++) {
+    numeros[i] = i+1;
+}
+
+console.log(numeros);
+
+let numerosX2 = [];
+
+for(i=0; i<10; i++) {
+    numerosX2[i] =   numeros[i] * 2;
+}
+
+console.log(numerosX2);
+
+
+
